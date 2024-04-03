1e758bdadb9f96cb3e2da6bcaa28b9d3670564d4
argo <argo@MAQUETA>
2024-04-03 13:00:39 +0200

    update

--- M03.UF5. AC1/Program.cs
+++ M03.UF5. AC1/Program.cs
@@ -8,15 +8,16 @@ namespace AC1
         public static void Main()
         {
             const int MAX = 10;
-            const string MsgPlayer = "Introduce el nombre (solo letras): ";
-            const string MsgMission = "Introduce la misión (nom adaptat de les lletres en grec, seguit d’un guió i un número de 3 xifres) : ";
-            const string MsgScore = "Introduce la puntuación [0-500]: ";
+            const string MsgPlayer = "Introdueix el nom (només lletres): ";
+            const string MsgMission = "Introdueix la missió (nom adaptat de les lletres en grec, seguit d’un guió i un número de 3 xifres) : ";
+            const string MsgScore = "Introdueix la puntuació [0-500]: ";
 
 
             List<Score> scores = new List<Score>();
 
             for (int i = 0; i < MAX; i++)
             {
+                Console.WriteLine($"Dades {i+1}");
                 Console.WriteLine(MsgPlayer);
                 string? player = Console.ReadLine();
                 Console.WriteLine(MsgMission);
@@ -27,6 +28,7 @@ namespace AC1
                 scores.Add(s);
                 
             }
+            Console.Clear();
 
             List<Score> uniqueScores = GenerateUniqueScores(scores);
             uniqueScores.Sort();
